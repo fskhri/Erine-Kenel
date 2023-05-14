b207cb5f59f0690574665f51b3de5b404a708a28
Richard Raya <rdxzv.dev@gmail.com>
2023-05-14 00:53:16 -0300

    build.sh: Switch to CROSS_COMPILE_COMPAT
    Change-Id: Id41580b00f16dbc274da404545f0dab1e5414e7d

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ fi
 MAKE_PARAMS="O=out ARCH=arm64 CC=clang LD=ld.lld LLVM=1 LLVM_IAS=1 \
     CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=arm-linux-gnueabi-"
+    CROSS_COMPILE_COMPAT=arm-linux-gnueabi-"
 
 export PATH="$TC_DIR/bin:$PATH"
 
