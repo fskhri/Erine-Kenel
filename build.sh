0bad97ea7571fb0cdd264202ff61b4aee0cb7705
Richard Raya <rdxzv.dev@gmail.com>
2023-07-07 00:24:51 -0300

    build.sh: Update and cleanup build script
    Change-Id: I4d1e5f6ed6a31a3ddf19b1f086e3ce809e5a2b49

--- build.sh
+++ build.sh
@@ -1,78 +1,77 @@
 #!/bin/bash
 #
-# Compile script for StormBreaker kernel
+# Compile script for perf kernel
 # Copyright (C) 2020-2023 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="StormBreaker-4.14-Surya-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="perf-surya-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$(pwd)/tc/clang-r450784e"
 AK3_DIR="$(pwd)/AnyKernel3"
 DEFCONFIG="vendor/surya-perf_defconfig"
 
 if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-    head=$(git rev-parse --verify HEAD 2>/dev/null); then
-    ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+   ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
 fi
 
 MAKE_PARAMS="O=out ARCH=arm64 CC=clang LD=ld.lld LLVM=1 LLVM_IAS=1 \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE=aarch64-linux-gnu- \
-    CROSS_COMPILE_COMPAT=arm-linux-gnueabi-"
+   CLANG_TRIPLE=aarch64-linux-gnu- \
+   CROSS_COMPILE=aarch64-linux-gnu- \
+   CROSS_COMPILE_COMPAT=arm-linux-gnueabi-"
 
 export PATH="$TC_DIR/bin:$PATH"
 
 if ! [ -d "$TC_DIR" ]; then
-	echo "AOSP clang not found! Cloning to $TC_DIR..."
-	if ! git clone --depth=1 -b 14 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
-		echo "Cloning failed! Aborting..."
-		exit 1
-	fi
+   echo "AOSP clang not found! Cloning to $TC_DIR..."
+   if ! git clone --depth=1 -b 14 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
+      echo "Cloning failed! Aborting..."
+      exit 1
+   fi
 fi
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
-    make $MAKE_PARAMS $DEFCONFIG savedefconfig
-    cp out/defconfig arch/arm64/configs/$DEFCONFIG
-    echo -e "\nSuccessfully regenerated defconfig at arch/arm64/configs/$DEFCONFIG"
-    exit
+   make $MAKE_PARAMS $DEFCONFIG savedefconfig
+   cp out/defconfig arch/arm64/configs/$DEFCONFIG
+   echo -e "\nSuccessfully regenerated defconfig at arch/arm64/configs/$DEFCONFIG"
+   exit
 fi
 
 if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
-    make $MAKE_PARAMS $DEFCONFIG
-    cp out/.config arch/arm64/configs/$DEFCONFIG
-    echo -e "\nSuccessfully regenerated full defconfig at arch/arm64/configs/$DEFCONFIG"
-    exit
+   make $MAKE_PARAMS $DEFCONFIG
+   cp out/.config arch/arm64/configs/$DEFCONFIG
+   echo -e "\nSuccessfully regenerated full defconfig at arch/arm64/configs/$DEFCONFIG"
+   exit
 fi
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
-    rm -rf out
-    echo "Cleaned output folder"
+   rm -rf out
+   echo "Cleaned output folder"
 fi
 
 mkdir -p out
 make $MAKE_PARAMS $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) $MAKE_PARAMS Image.gz dtbo.img 2> >(tee log.txt >&2) || exit $?
+make -j$(nproc --all) $MAKE_PARAMS Image.gz dtb.img dtbo.img 2> >(tee log.txt >&2) || exit $?
 
 kernel="out/arch/arm64/boot/Image.gz"
-dtb="out/arch/arm64/boot/dts/qcom/sdmmagpie.dtb"
+dtb="out/arch/arm64/boot/dtb.img"
 dtbo="out/arch/arm64/boot/dtbo.img"
 
 if [ ! -f "$kernel" ] || [ ! -f "$dtb" ] || [ ! -f "$dtbo" ]; then
-    echo -e "\nCompilation failed!"
-    exit 1
+   echo -e "\nCompilation failed!"
+   exit 1
 fi
 
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
 if [ -d "$AK3_DIR" ]; then
-    cp -r $AK3_DIR AnyKernel3
-    git -C AnyKernel3 checkout surya &> /dev/null
-elif ! git clone -q https://github.com/stormbreaker-project/AnyKernel3 -b surya; then
-    echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
-    exit 1
+   cp -r $AK3_DIR AnyKernel3
+   git -C AnyKernel3 checkout master &> /dev/null
+elif ! git clone -q https://github.com/rd-stuffs/AnyKernel3 -b master; then
+     echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+     exit 1
 fi
-cp $kernel $dtbo AnyKernel3
-cp $dtb AnyKernel3/dtb
+cp $kernel $dtb $dtbo AnyKernel3
 rm -rf out/arch/arm64/boot
 cd AnyKernel3
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
