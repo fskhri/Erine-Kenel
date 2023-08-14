75fc38e4187b70b51e8560e3994d854b1d525f21
Richard Raya <rdxzv.dev@gmail.com>
2023-08-14 18:27:43 +0000

    exec: Guard Simple LMK hack
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Change-Id: Idbc3936ab9fcff84b8e93d826ac96d5733ddc81d

--- fs/exec.c
+++ fs/exec.c
@@ -1868,6 +1868,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 		bprm.argc = 1;
 	}
 
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
 	// Super nasty hack to disable lmkd reloading props
 	if (unlikely(strcmp(bprm.filename, "/system/bin/lmkd") == 0)) {
 		if (is_lmkd_reinit(&argv)) {
@@ -1876,6 +1877,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 			goto out;
 		}
 	}
+#endif
 
 	retval = exec_binprm(&bprm);
 	if (retval < 0)
